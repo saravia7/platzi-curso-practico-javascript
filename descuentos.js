3aac6b9897bc0d1b960e135595c9ab6e1c956a43
Sabrina saravia <sabry.bksaravia3@gmail.com>
2021-09-08 15:48:01 -0600

    Funcion para calcular la mediana

--- descuentos.js
+++ descuentos.js
@@ -1,45 +1,45 @@
 //const precioOriginal = 120;
 //const descuento =18;
 
-function calcularPrecioConDescuento(precio,descuento){
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
-    return precioConDescuento;
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
 };
 
 const coupons = [
-    "JuanDC_es_Batman",
-    "pero_no_le_digas_a_nadie",
-    "es_un_secreto",
+  "JuanDC_es_Batman",
+  "pero_no_le_digas_a_nadie",
+  "es_un_secreto",
 ];
 
 function onClickButtonPriceDiscount() {
-    const inputPrice = document.getElementById("InputPrice");
-    const priceValue = inputPrice.value;
-    
-    const inputCoupon = document.getElementById("InputCoupon");
-    const couponValue = inputCoupon.value;
-  
-    let descuento;
-  
-    switch(couponValue) {
-      case "JuanDC_es_Batman":
-        descuento = 15;
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
+
+  let descuento;
+
+  switch (couponValue) {
+    case "JuanDC_es_Batman":
+      descuento = 15;
       break;
-      case "pero_no_le_digas_a_nadie":
-        descuento = 30;
+    case "pero_no_le_digas_a_nadie":
+      descuento = 30;
       break;
-      case "es_un_secreto":
-        descuento = 25;
+    case "es_un_secreto":
+      descuento = 25;
       break;
-    }
-  
-  
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, couponValue);
-     
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
   }
+
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, couponValue);
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
 
 //console.log({
     //precioOriginal,
--- promedio.js
+++ promedio.js
@@ -2,20 +2,20 @@
 
 
 
-function calcularMediaAritmetica(lista){
-    
+function calcularMediaAritmetica(lista) {
+
     //let sumaLista = 0;
 
-   // for(let i = 0; i < lista.length;i++){
-   //   sumaLista = sumaLista + lista[i];
-   // }
-    
-   const sumaLista = lista.reduce(
-       function (valorAcumulado = 0,nuevoElemento){
-          return valorAcumulado + nuevoElemento;       
+    // for(let i = 0; i < lista.length;i++){
+    //   sumaLista = sumaLista + lista[i];
+    // }
 
-       }
-   );
-   const promedioLista = sumaLista / lista.length;
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
     return promedioLista;
 }
