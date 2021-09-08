dddb557babeb16885aebbf7cf92cda4c0894afd7
Sabrina saravia <sabry.bksaravia3@gmail.com>
2021-09-08 14:27:44 -0600

    Funcion para calcular un precio con cupones de descuento

--- descuentos.js
+++ descuentos.js
@@ -5,9 +5,41 @@ function calcularPrecioConDescuento(precio,descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
     return precioConDescuento;
-}
+};
 
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
 
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+  
+    let descuento;
+  
+    switch(couponValue) {
+      case "JuanDC_es_Batman":
+        descuento = 15;
+      break;
+      case "pero_no_le_digas_a_nadie":
+        descuento = 30;
+      break;
+      case "es_un_secreto":
+        descuento = 25;
+      break;
+    }
+  
+  
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
 
 //console.log({
     //precioOriginal,
