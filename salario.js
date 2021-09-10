19bddf74d1fb4eb9a15463983951daaf034afb89
Sabrina saravia <sabry.bksaravia3@gmail.com>
2021-09-10 12:54:40 -0600

    funcion para calcular salario

---
+++ salario.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Natalia",
+    salary: 1500,
+});
+colombia.push({
+    name: "Fanny",
+    salary: 1800,
+});
+colombia.push({
+    name: "Alexa",
+    salary: 1000,
+});
+colombia.push({
+    name: "Patricia",
+    salary: 1800,
+});
+colombia.push({
+    name: "Estela",
+    salary: 1900,
+});
+colombia.push({
+    name: "Nicol",
+    salary: 1700,
+});
+colombia.push({
+    name: "Carolina",
+    salary: 1600,
+});
+colombia.push({
+    name: "Daniel",
+    salary: 1500,
+});
+colombia.push({
+    name: "Morelia",
+    salary: 1400,
+});
+colombia.push({
+    name: "David",
+    salary: 1300,
+});
+colombia.push({
+    name: "Giselle",
+    salary: 1200,
+});
+colombia.push({
+    name: "Alan",
+    salary: 1100,
+});
+colombia.push({
+    name: "Saidy",
+    salary: 1000,
+});
+colombia.push({
+    name: "Emely",
+    salary: 1000,
+});
+colombia.push({
+    name: "Ever",
+    salary: 1000,
+});
+colombia.push({
+    name: "Elvia",
+    salary: 1000,
+});
+colombia.push({
+    name: "Roberto",
+    salary: 1000,
+});
+colombia.push({
+    name: "Jonathan",
+    salary: 1000,
+});
+colombia.push({
+    name: "Roxana",
+    salary: 1000,
+});
